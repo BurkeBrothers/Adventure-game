8d0b434ca63b0cc1d8f0ffa5be458b7039124439
BurkeBrothers <42327724+BurkeBrothers@users.noreply.github.com>
2018-12-05 18:32:00 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -10,28 +10,12 @@ with open("Bank.json", 'r') as f:
 with open("Commands.json", 'r') as f:
     commands = json.load(f)
 
-print(Store.vpn())
-print("--------------------------")
-print(Store.HackingPack())
+for program, price in Store_Items.items():
+    print(f"{program} | ${price}")
 
-def store():
-    while True:
-        print("\nWhat do you want to buy?\n")
-        time.sleep(x)
-        choice = input(">> ")
-        if choice == "1":
-            if bank["Bank"]["Money"] >= vpnPrice:
-                commands["Test"]["InstalledCommands"]["Command1"] = "vpn"
-            with open("Commands.json", 'w') as f:
-                    commands = json.dump(commands, f)
+current_commands = commands["Test"]["InstalledCommands"]
 
-            if bank["Bank"]["Money"] < vpnPrice:
-                print("You cant buy this tool")
+for command in current_commands.items():
+    print(*command)
 
-        if choice == "2":
-            if bank["Bank"]["Money"] >= HackingPackPrice:
-                commands["Test"]["Commands"]["InstalledCommands"]["Command2"] = "HackingPack"
-                with open("Commands.json", 'w') as f:
-                        commands = json.dump(commands, f)
-            if bank["Bank"]["Money"] < HackingPackPrice:
-                print("You cant buy this tool")
+
