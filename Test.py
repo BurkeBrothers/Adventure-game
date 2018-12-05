8b49598e844182f84ed03a9a2fcebdaec2c878e5
BurkeBrothers <42327724+BurkeBrothers@users.noreply.github.com>
2018-12-05 20:37:37 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,17 +5,24 @@ from StoreItems import *
 
 x = .5
 
-with open("Bank.json", 'r') as f:
-    bank = json.load(f)
-with open("Commands.json", 'r') as f:
+with open("User.json", 'r') as f:
     commands = json.load(f)
+with open("User.json", 'r') as f:
+    money = json.load(f)
+current_commands = commands["User"]["Test"]["InstalledCommands"]
 
 for program, price in Store_Items.items():
-    print(f"{program} | ${price}")
+    print(f"{program:<10} | ${price}")
+    #
+    # if program in current_commands:
+    #     print(f"{program} is in players inventory")
+    # else:
+    #     print(f"{program} is not in players inventory")
 
-current_commands = commands["Test"]["InstalledCommands"]
 
-for command in current_commands.items():
-    print(*command)
-
-
+def store():
+    while True:
+        print("\nWhat do you want to buy?\n")
+        time.sleep(x)
+        choice = input(">> ")
+store()
